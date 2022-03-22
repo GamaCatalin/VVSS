156087d03f199f9136bbc79d62e117a4293a8690
Catalin Gama <catalin.gama@ngi.systems>
2022-03-22 15:52:17 +0200

    sigur acum

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,7 +1,9 @@
 package service;
 
 import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
 import validation.StudentValidator;
+import validation.TemaValidator;
 import validation.ValidationException;
 
 public class ServiceTest {
@@ -44,4 +46,45 @@ public class ServiceTest {
         assert(initLen[0] == finLen[0]);
         service.deleteStudent("999");
     }
+
+
+    @org.junit.jupiter.api.Test
+    public void addAssignment_Added() {
+
+
+        Service service = new Service(null,new TemaXMLRepository(new TemaValidator(),"test_assignment.xml"),null);
+
+        int initLen[] = {0};
+        service.findAllTeme().forEach(s -> initLen[0]++);
+        assert(service.saveTema("999","asta merge",13,7) == 1);
+        int finLen[] = {0};
+        service.findAllTeme().forEach(s -> finLen[0]++);
+
+        assert(initLen[0] + 1 == finLen[0]);
+
+        service.deleteTema("999");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void addAssignment_NotAdded() {
+
+        Service service = new Service(null,new TemaXMLRepository(new TemaValidator(),"test_assignment.xml"),null);
+
+        int initLen[] = {0};
+        service.findAllTeme().forEach(s -> initLen[0]++);
+        try{
+            service.saveTema("999","nu merge", 7,13);
+        }
+        catch (ValidationException e){
+            assert(true);
+        }
+        int finLen[] = {0};
+        service.findAllTeme().forEach(s -> finLen[0]++);
+
+        assert(initLen[0] == finLen[0]);
+        service.deleteTema("999");
+    }
+
+
 }
