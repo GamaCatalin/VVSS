2ef63d15b6a7723d282012346c456ce7acfab38b
Catalin Gama <catalin.gama@ngi.systems>
2022-04-05 16:01:14 +0300

    integration change

--- src/test/java/service/GradeServiceTest.java
+++ src/test/java/service/GradeServiceTest.java
@@ -78,9 +78,58 @@ public class GradeServiceTest {
     @org.junit.jupiter.api.Test
     public void addGrade_Integration() {
 
-        saveStudent_Added();
-        addAssignment_Added();
-        addGrade_Added();
+        Service service = new Service(new StudentXMLRepository(new StudentValidator(),"test_students.xml"),
+                new TemaXMLRepository(new TemaValidator(),"test_assignment.xml"),
+                new NotaXMLRepository(new NotaValidator(),"test_grade.xml"));
+
+
+        String idStudent = "999";
+        String nameStudent = "Gioni Bravo";
+        String idAssignment = "999";
+        String descriptionAssignment = "asta merge";
+        int deadlineAssignment = 13;
+        int startlineAssignmenet = 7;
+        int groupStudent = 910;
+        int valGrade = 10;
+        int turnIn = 14;
+        String feedback = "e bun";
+
+
+        service.deleteNota(idStudent, idAssignment);
+        service.deleteStudent(idStudent);
+        service.deleteTema(idAssignment);
+
+
+        int initLen[] = {0,0,0};
+        int finLen[] = {0,0,0};
+
+
+
+        service.findAllStudents().forEach(s -> initLen[0]++);
+        assert(service.saveStudent(idStudent,nameStudent,groupStudent) == 1);
+        service.findAllStudents().forEach(s -> finLen[0]++);
+        assert(initLen[0] + 1 == finLen[0]);
+
+
+
+
+
+        service.findAllTeme().forEach(s -> initLen[1]++);
+        assert(service.saveTema(idAssignment,descriptionAssignment,deadlineAssignment,startlineAssignmenet) == 1);
+        service.findAllTeme().forEach(s -> finLen[1]++);
+        assert(initLen[1] + 1 == finLen[1]);
+
+
+
+
+
+
+
+
+        service.findAllNote().forEach(s -> initLen[2]++);
+        assert(service.saveNota(idStudent,idAssignment, valGrade, turnIn, feedback) == 1);
+        service.findAllNote().forEach(s -> finLen[2]++);
+        assert(initLen[2] + 1 == finLen[2]);
 
     }
 
